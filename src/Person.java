3faa4f889ae7aa27ca19b2f9fa2edb10b33d1687
bjl0615 <bjl0615@gmail.com>
2022-09-07 22:41:03 +0900

    Change this

--- src/Person.java
+++ src/Person.java
@@ -4,6 +4,30 @@ public class Person {
     private int cashAmount; // 잔액
     private BankAccount account; // 계정
 
+    public Person(String name, int age) {
+        if (age < 0) {
+            this.age = 12;
+        } else {
+            this.age =  age;
+        }
+        this.name = name;
+    }
+
+    public Person(String name , int age, int cashAmount) {
+        if(age < 0) {
+            this.age = 12;
+        } else {
+            this.age = age;
+        }
+
+        if(cashAmount < 0) {
+            this.cashAmount = 0;
+        }  else {
+            cashAmount = age;
+        }
+        this.name = name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
