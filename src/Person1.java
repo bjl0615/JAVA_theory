c8fbcb27e223938b412f187cf577fff3feaf821a
bjl0615 <bjl0615@gmail.com>
2022-09-13 23:37:07 +0900

    Person1.java

---
+++ src/Person1.java
@@ -0,0 +1,25 @@
+public class Person1 {
+    private String name;
+    private int age;
+
+    Person1(String name , int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
