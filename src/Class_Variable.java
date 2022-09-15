d44fef589d6d657e0296dd2370721b0b93d17bee
bjl0615 <bjl0615@gmail.com>
2022-09-15 19:38:56 +0900

    Variable

---
+++ src/Class_Variable.java
@@ -0,0 +1,29 @@
+class Person2 {
+    static int count;
+
+    public Person2() {
+        count++;
+    }
+}
+
+public class Class_Variable {
+    public static void main(String[] args) {
+//        Person2 p1 = new Person2();
+//        Person2.count++;
+//
+//        Person2 p2 = new Person2();
+//        Person2.count++;
+//
+//        Person2 p3 = new Person2();
+//        Person2.count++;
+//
+//        Person2 p4 = new Person2();
+//        Person2.count++;
+//
+//        System.out.println(Person2.count);
+        Person2 p1 = new Person2();
+        Person2 p2 = new Person2();
+        Person2 p3 = new Person2();
+        Person2 p4 = new Person2();
+    }
+}
