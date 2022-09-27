156122e30ce908f824b73e17d55e9492a442f85b
bongjun <bjl0615@gmail.com>
2022-09-27 20:40:04 +0900

    SavingsAccount

---
+++ src/SavingsAccount.java
@@ -0,0 +1,15 @@
+public class SavingsAccount extends BankAccount {
+    private double interest;
+
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void addInterest() {
+        setBalance((int) (getBalance() * (1 + interest)));;
+    }
+}
