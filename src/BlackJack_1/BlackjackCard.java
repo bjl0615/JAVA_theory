88236e37c574d94e23322fd568de499b247e9cf9
bongjun <bjl0615@gmail.com>
2022-09-28 21:36:40 +0900

    BlackJack

---
+++ src/BlackJack_1/BlackjackCard.java
@@ -0,0 +1,28 @@
+package BlackJack_1;
+
+public class BlackjackCard extends Card{
+    public BlackjackCard(int suitNumber, int rankNumber) {
+        super(suitNumber, rankNumber);
+    }
+
+    public int getValue() {
+        // rank를 가준
+        // Ace(1)은 11을 리턴
+        // J, Q, K(11-13)은 10을 리턴
+
+        switch(rankNumber) {
+            case 1 :
+                return 11;
+            case 11 :
+            case 12 :
+            case 13 :
+                return 10;
+            default:
+                return rankNumber;
+        }
+    }
+
+    public boolean isAce() {
+        return rankNumber == 1;
+    }
+}
