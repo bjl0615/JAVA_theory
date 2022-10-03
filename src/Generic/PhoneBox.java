b56fdbda9b12e7b5e2bc661aa6e69773fb4eeb22
bjl0615 <bjl0615@gmail.com>
2022-10-03 22:36:44 +0900

    Generic

---
+++ src/Generic/PhoneBox.java
@@ -0,0 +1,13 @@
+package Generic;
+
+public class PhoneBox {
+    private String object;
+
+    public void set(String object) {
+        this.object = object;
+    }
+
+    public String get() {
+        return object;
+    }
+}
