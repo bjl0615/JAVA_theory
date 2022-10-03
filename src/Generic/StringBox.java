eb5605ce2a779419f1f69297a39be7d0ad0f4042
bjl0615 <bjl0615@gmail.com>
2022-10-03 22:36:52 +0900

    Generic

---
+++ src/Generic/StringBox.java
@@ -0,0 +1,13 @@
+package Generic;
+
+public class StringBox {
+    private String object;
+
+    public void set(String obejct) {
+        this.object = obejct;
+    }
+
+    public String get() {
+        return object;
+    }
+}
